e2e2681626263564f24bce6304e193aaa250f54c
mrhuangqiwei <973414519@qq.com>
2015-02-04 18:13:14 +0800

    first

---
+++ src/com/example/viewpagerdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.viewpagerdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+	/**
+	 * ViewPager
+	 */
+	private ViewPager viewPager;
+	
+	/**
+	 * 装点点的ImageView数组
+	 */
+	private ImageView[] tips;
+	
+	/**
+	 * 装ImageView数组
+	 */
+	private ImageView[] mImageViews;
+	
+	/**
+	 * 图片资源id
+	 */
+	private int[] imgIdArray ;
+	private View view1;
+	private View view2;
+	private View view3;
+	  ArrayList<View>    viewList = new ArrayList<View>();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+   
+  
+
+	}
+		
+		
+		
+		
+		
+		
+	
+
+}
